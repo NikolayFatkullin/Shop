7098b02c44ec36028b58b1d30c733da21a79285b
nikelok01@gmail.com <163amefeZ>
2021-01-11 20:15:48 +0200

    - Deleted id from constructor in class Car

--- src/main/java/com/shop/model/Car.java
+++ src/main/java/com/shop/model/Car.java
@@ -8,8 +8,7 @@ public class Car {
     private Manufacturer manufacturer;
     private List<Driver> drivers;
 
-    public Car(Long id, String model, Manufacturer manufacturer, List<Driver> drivers) {
-        this.id = id;
+    public Car(String model, Manufacturer manufacturer, List<Driver> drivers) {
         this.model = model;
         this.manufacturer = manufacturer;
         this.drivers = drivers;
